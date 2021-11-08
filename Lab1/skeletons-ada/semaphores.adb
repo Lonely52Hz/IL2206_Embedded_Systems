32d06b7041963b5ce8e4052fab5b58d09abc400e
Lonely52Hz <49867212+Lonely52Hz@users.noreply.github.com>
2021-11-08 22:02:36 +0100

    Add files via upload

---
+++ Lab1/skeletons-ada/semaphores.adb
@@ -0,0 +1,22 @@
+-- Package: Semaphores
+--
+-- ==> Complete the code at the indicated places
+
+package body Semaphores is
+   protected body CountingSemaphore is
+      entry Wait -- To be completed
+         when Count > 0 is -- Guard
+      begin 
+         Count := Count - 1;
+      end Wait;
+
+
+      entry Signal -- To be completed
+         when Count < MaxCount is -- Guard
+      begin
+         Count := Count + 1;
+      end Signal;
+
+   end CountingSemaphore;
+end Semaphores;
+
